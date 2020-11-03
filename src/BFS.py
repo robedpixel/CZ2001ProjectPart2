8770f05f63730081b6e67990b22ed7be0d38a096
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2020-11-03 18:06:04 +0800

    added function for constant time complexity multi bfs search

--- src/BFS.py
+++ src/BFS.py
@@ -16,27 +16,65 @@ queue = []  # Initialize a queue
 check = [False, False, True, False, False, False];
 nodenum = [];
 
+def snap_bfs_shortest_path_constant(graph: snap.PUNGraph, hospital_locations_list: list, num_hospitals_to_search: int) -> typing.Tuple[
+    int, list]:
+    numnodes = graph.GetNodes()
+    visitedarray = np.zeros((numnodes, 1), dtype=bool)
+    output = list()
+    # maintain a queue of paths
+    queue = list()
+    # push the hospital nodes into the queue
 
-def snap_bfs_shortest_path(starting_node: int, graph: snap.PUNGraph, hospital_locations_list: list) -> typing.Tuple[
+    for hospital in hospital_locations_list:
+        queue.append([hospital])
+        visitedarray[hospital] = True
+        output.append([hospital])
+
+    while queue:
+        # get the first path from the queue
+        path = queue.pop(0)
+        # get the last node from the path
+        node = path[-1]
+        print("Current node: " + str(node))
+        # enumerate all adjacent nodes, construct a new path and push it into the queue
+        current_node = graph.GetNI(int(node))
+        for Id in current_node.GetOutEdges():
+            if not visitedarray[Id]:
+                # if not visitedarray[Id]:
+                # visitedarray[Id] = True
+                visitedarray[Id] = True
+                new_path = list(path)
+                new_path.append(Id)
+                queue.append(new_path)
+                output.append(new_path)
+                print("edge (%d %d) not visited yet" % (current_node.GetId(), Id))
+    return output
+
+def snap_bfs_shortest_path(starting_node: int, graph: snap.PUNGraph, hospital_locations_list: list, num_hospitals_to_search: int) -> typing.Tuple[
     int, list]:
     """
     This function does breadth first search on a snap.py graph, it returns the node at which it has found a hospital
     and a list of the shortest path
     """
     # Create array of same size as number of nodes of graph
-    numnodes = graph.GetNodes()
-    hospitalarray = np.zeros((numnodes, 1), dtype=bool)
-    visitedarray = np.zeros((numnodes, 1), dtype=bool)
-    # load array with values in hospital list
-    for location in hospital_locations_list:
-        hospitalarray[location] = True
+    #numnodes = graph.GetNodes()
+    #hospitalarray = np.zeros((numnodes, 1), dtype=bool)
+    #visitedarray = np.zeros((numnodes, 1), dtype=bool)
 
-    visitedarray[starting_node] = True
+    output = list();
+    visitedarray = list()
+    found = 0
+    #visitedarray.append(starting_node)
+    #visitedarray[starting_node] = True
 
     # maintain a queue of paths
     queue = list()
-    # push the first path into the queue
-    queue.append([starting_node])
+    # push the hospital nodes into the queue
+
+    for hospital in hospital_locations_list:
+        queue.append([hospital])
+        visitedarray.append([hospital, hospital])
+
     while queue:
         # get the first path from the queue
         path = queue.pop(0)
@@ -44,43 +82,24 @@ def snap_bfs_shortest_path(starting_node: int, graph: snap.PUNGraph, hospital_lo
         node = path[-1]
         print("Current node: " + str(node))
         # path found
-        if hospitalarray[node]:
-            print("Hospital found at node: ", str(node))
-            return node, path
+        if node == starting_node:
+            print("Hospital path found")
+            output.append(path)
+            found += 1
+            if found == num_hospitals_to_search:
+                return output
         # enumerate all adjacent nodes, construct a new path and push it into the queue
         current_node = graph.GetNI(int(node))
         for Id in current_node.GetOutEdges():
-            if not visitedarray[Id]:
-                visitedarray[Id] = True
+            if [path[0], Id] not in visitedarray:
+            # if not visitedarray[Id]:
+                #visitedarray[Id] = True
+                visitedarray.append([path[0], Id])
                 new_path = list(path)
                 new_path.append(Id)
                 queue.append(new_path)
                 print("edge (%d %d) not visited yet" % (current_node.GetId(), Id))
-    return -1, list()
-
-
-def snap_bfs_top_k_shortest(starting_node: int, graph: snap.PUNGraph, hospital_locations_list: list,
-                            paths_to_find: int, bool_save_to_file: bool) -> list:
-    """
-    This function returns a list of paths from 1 node to the nearest k hospitals,
-    uses snap_bfs_shortest_path function to check the nearest path to each hospital
-    """
-    list_of_paths = list()
-    p_hospital_locations_list = hospital_locations_list.copy()
-    if paths_to_find < 0:
-        print("can't find zero or negative paths!")
-        return
-    if paths_to_find > len(hospital_locations_list):
-        print("can't find more paths than there are hospitals!")
-        return
-    for x in range(paths_to_find):
-        hospitalnode, path = snap_bfs_shortest_path(starting_node, graph, p_hospital_locations_list)
-        if path:
-            list_of_paths.append(path)
-            p_hospital_locations_list.remove(hospitalnode)
-    if bool_save_to_file:
-        output_to_file(list_of_paths)
-    return list_of_paths
+    return output
 
 
 def complete_snap_bfs_top_k_shortest(graph: snap.PUNGraph, hospital_locations_list: list,
@@ -91,8 +110,10 @@ def complete_snap_bfs_top_k_shortest(graph: snap.PUNGraph, hospital_locations_li
     """
     outputlist = list()
     for NI in graph.Nodes():
-        paths = snap_bfs_top_k_shortest(NI.GetId(), graph, hospital_locations_list, paths_to_find, bool_save_to_file)
+        paths = snap_bfs_shortest_path(NI.GetId(), graph, hospital_locations_list, paths_to_find)
         outputlist.extend(paths)
+        if bool_save_to_file:
+            output_to_file(paths)
     return outputlist
 
 
@@ -103,8 +124,9 @@ def output_to_file(bfs_path_list: list):
     """
     file1 = open("output.txt", "a")
     for path in bfs_path_list:
-        file1.write("From node " + str(path[0]) + " to hospital at node " + str(path[-1]) + "\n")
+        file1.write("From node " + str(path[-1]) + " to hospital at node " + str(path[0]) + "\n")
         file1.write("Distance: " + str(len(path)-1) + "\n")
+        path.reverse()
         file1.write("Path: " + str(path) + "\n")
         file1.write("\n")
     file1.close()
--- src/TestingConsole.py
+++ src/TestingConsole.py
@@ -1,19 +1,21 @@
 import snap
-from BFS import snap_bfs_top_k_shortest, output_to_file, complete_snap_bfs_top_k_shortest
+from BFS import snap_bfs_shortest_path_constant, snap_bfs_shortest_path, output_to_file, complete_snap_bfs_top_k_shortest
 from RandomGraphGenerator import RandomGraphGenerator
 from RandomHospitalLocationGen import gen_ran_hospital_locations
 import time
 
 hospitallist = list()
-num_nodes = 400
-num_edges = 800
-num_hospitals = 8
+num_nodes = 100
+num_edges = 200
+num_hospitals_to_search = 4
+num_hospitals = 16
 graphobject = RandomGraphGenerator(num_nodes, num_edges)
 randomgraph = graphobject.retrieve_random_graph()
 hospitallist = gen_ran_hospital_locations(num_nodes, num_hospitals)
 start_time = time.time()
-pathlist = snap_bfs_top_k_shortest(1, randomgraph, hospitallist, 4, True)
-#pathlist = complete_snap_bfs_top_k_shortest(randomgraph, hospitallist, 4, False)
+pathlist = snap_bfs_shortest_path_constant(randomgraph,hospitallist,num_hospitals_to_search)
+#pathlist = complete_snap_bfs_top_k_shortest(randomgraph, hospitallist, num_hospitals_to_search, False)
 end_time = time.time()
-
+for i in pathlist:
+    print(i)
 print("---time taken: %s seconds ---" % (end_time - start_time))
